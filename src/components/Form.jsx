832eeeb3fe6e7ee7a4aec0497a58d8c554dc22a9
Johiny <johiny7@live.com>
2023-02-08 21:20:46 -0500

    test 7

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,78 +1,104 @@
-import emailjs from "@emailjs/browser"
-import { useRef, useState } from 'react'
-import ReCAPTCHA from 'react-google-recaptcha'
-import ContactLoader from '../components/ContactLoader'
-import { ToastContainer, toast } from 'react-toastify';
-const Form = () => {
+import emailjs from "@emailjs/browser";
+import { useRef, useState } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
+import ContactLoader from "../components/ContactLoader";
+import { toast } from "react-toastify";
 
+const Form = () => {
   // manejo del formulario y uso de emailjs
-  const form = useRef(null)
+  const form = useRef(null);
   const sendEmail = (e) => {
-    e.preventDefault()
-    setLoading(true)
+    e.preventDefault();
+    setLoading(true);
     // se envia todo lo que necesita el servicio de emailjs incluyendo la key que devuelve la recptcha
-    emailjs.sendForm(import.meta.env.REACT_APP_EMAILJS_SERVICE_ID, "contact_form", form.current, import.meta.REACT_APP_EMAILJS_PUBLICKEY)
-    .then((result) => {
-      setLoading(false)
-      toast.success('Mensaje enviado', {
-        position: "bottom-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true
-        });
-    }, (error) => {
-      setLoading(false)
-      console.log(error)
-      toast.error('El mensaje no se pudo enviar', {
-        position: "bottom-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true
-        });
-    });
-  }
+    emailjs
+      .sendForm(
+        import.meta.env.REACT_APP_EMAILJS_SERVICE_ID,
+        "contact_form",
+        form.current,
+        import.meta.REACT_APP_EMAILJS_PUBLICKEY
+      )
+      .then(
+        (result) => {
+          setLoading(false);
+          toast.success("Mensaje enviado", {
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.log(error);
+          toast.error("El mensaje no se pudo enviar", {
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+          });
+        }
+      );
+  };
   // Estados formulario
-  const [Loading,setLoading] = useState(false);
-  const [submitButton,setSubmitButton] = useState(false);
+  const [Loading, setLoading] = useState(false);
+  const [submitButton, setSubmitButton] = useState(false);
   // const captcha = useRef()
   // manejo del captcha
   const captchaChange = () => {
-    setSubmitButton(!submitButton)
-  }
+    setSubmitButton(!submitButton);
+  };
 
+  return (
+    <>
+      <form onSubmit={sendEmail} ref={form} className="callback-form">
+        <div className="form-control">
+          <label htmlFor="user_name"></label>
+          <input
+            type="text"
+            name="user_name"
+            id="name"
+            required
+            placeholder="Su nombre"
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="user_email"></label>
+          <input
+            type="email"
+            name="user_email"
+            id="email"
+            required
+            placeholder="Su correo electronico"
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="comment"></label>
+          <textarea
+            name="message"
+            id="comment"
+            required
+            rows="4"
+            cols="39"
+            placeholder="Escriba su comentario"
+          ></textarea>
+        </div>
+        {/* captcha de google que cambia de valor cuando es resolvida y ese valor se guarda en los valores del formulario */}
+        <ReCAPTCHA
+          sitekey={import.meta.env.PUBLIC_RECAPTCHA_SITE_KEY}
+          onChange={captchaChange}
+        />
+        {submitButton === true ? (
+          <input type="submit" value="Enviar" id="submit" className="btn" />
+        ) : null}
+      </form>
+      {Loading ? <ContactLoader /> : null}
+    </>
+  );
+};
 
-  return ( 
-   <>
-        <form onSubmit={sendEmail} ref={form} className="callback-form">
-          <div className="form-control">
-            <label htmlFor="user_name"></label>
-            <input type="text" name="user_name" id="name" required
-            placeholder="Su nombre" />
-          </div>
-          <div className="form-control">
-            <label htmlFor="user_email"></label>
-            <input type="email" name="user_email" id="email" required
-            placeholder="Su correo electronico" />
-          </div>
-          <div className="form-control">
-            <label htmlFor="comment"></label>
-            <textarea name="message" id="comment" required rows="4" cols="39" placeholder='Escriba su comentario'>
-            </textarea>
-          </div>
-          {/* captcha de google que cambia de valor cuando es resolvida y ese valor se guarda en los valores del formulario */}
-          <ReCAPTCHA
-            sitekey={import.meta.env.PUBLIC_RECAPTCHA_SITE_KEY}
-            onChange={captchaChange}/>
-          {submitButton === true?<input type="submit" value="Enviar" id= "submit"
-          className="btn"/>:null}
-        </form>
-      {Loading ? <ContactLoader/> : null}
-  </>
-  )
-}
-
-export default Form
+export default Form;
